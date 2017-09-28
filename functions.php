0e31aca11ef5c3e3eae450e20b430650d5160492
Charley Walton <chazeah@gmail.com>
2017-09-28 10:09:19 +0800

    removing website from comment form

--- functions.php
+++ functions.php
@@ -1,6 +1,5 @@
 <?php
 function my_theme_enqueue_styles() {
-
     $parent_style = 'twentyfifteen-style';
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
@@ -11,4 +10,12 @@ function my_theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+// Remove URL field from comments
+function remove_url_comments($fields) {
+    unset($fields['url']);
+    return $fields;
+}
+add_filter('comment_form_default_fields', 'remove_url_comments');
+
 ?>
