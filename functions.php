568e99d9a6d058841bfce09a49269d1c667213d7
Charley Walton <chazeah@gmail.com>
2017-10-15 11:32:29 +0800

    small whitespace changes

--- functions.php
+++ functions.php
@@ -1,22 +1,23 @@
 <?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     $parent_style = 'twentyfifteen-style';
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
+    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style(
+        'child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
+        array($parent_style),
         wp_get_theme()->get('Version')
     );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 // Remove URL field from comments.
+add_filter('comment_form_default_fields', 'remove_url_comments');
 function remove_url_comments($fields) {
     unset($fields['url']);
     return $fields;
 }
-add_filter('comment_form_default_fields', 'remove_url_comments');
 
 // Remove built-in RSS feeds.
 add_action('after_setup_theme', 'polyfecta_feed_setup' , 11);
@@ -34,10 +35,10 @@ function add_polyfecta_rss_link() {
 add_filter('the_category_rss', '__return_empty_string');
 
 // Show the full content in the RSS feed's "description".
+add_filter('the_excerpt_rss', 'show_full_content_in_description', 10, 1);
 function show_full_content_in_description($excerpt){
     return get_the_content_feed();
 }
-add_filter('the_excerpt_rss', 'show_full_content_in_description', 10, 1);
 
 // Enable shortcodes in text widgets.
 add_filter('widget_text', 'do_shortcode');
