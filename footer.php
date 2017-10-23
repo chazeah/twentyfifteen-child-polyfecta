94485b124a43109868f488ba177cc81d0e7366ca
Charley Walton <chazeah@gmail.com>
2017-10-23 16:39:31 +0800

    adding simple footer

--- footer.php
+++ footer.php
@@ -6,6 +6,27 @@
 
     </div><!-- .site-content -->
 
+    <footer id="colophon" class="site-footer" role="contentinfo">
+        <div class="site-info">
+            <a href="/about/">About</a>
+            &nbsp;&nbsp;•&nbsp;&nbsp;
+            <a href="/email/">Subscribe</a>
+            &nbsp;&nbsp;•&nbsp;&nbsp;
+            <a
+                class="copyright-link"
+                href="https://creativecommons.org/licenses/by-nc/4.0/"
+            >
+                <img
+                    alt="Create Commons Attribution-NonCommercial 4.0 International"
+                    title="Create Commons Attribution-NonCommercial 4.0 International"
+                    src="/wp-content/uploads/2017/10/by-nd-cc-license-compact.svg"
+                    height="15"
+                    width="80"
+                >
+            </a>
+        </div><!-- .site-info -->
+    </footer><!-- .site-footer -->
+
 </div><!-- .site -->
 
 <?php wp_footer(); ?>
